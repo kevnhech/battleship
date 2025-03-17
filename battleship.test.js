5edf9e4436130f91846b2bf0aefbd1e9961121c0
kevin <kevnhech@gmail.com>
2025-03-17 13:09:48 -0700

    Create a Ship class and add testing

---
+++ battleship.test.js
@@ -0,0 +1,10 @@
+const Ship = require('./battleship');
+
+test('Your ships will be objects that include their length, times hit, and sunk', () => {
+  let p = new Ship();
+  p.hit();
+  p.hit();
+  p.hit();
+  p.isSunk();
+  expect(p).toEqual({ length: 3, hitCount: 3, sunk: true });
+});
