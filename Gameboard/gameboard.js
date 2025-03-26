b9ade68b85aaf8f4a065a71649a3186662433371
kevin <kevnhech@gmail.com>
2025-03-26 10:13:19 -0700

    feat: place ships at specific coordinates

--- Gameboard/gameboard.js
+++ Gameboard/gameboard.js
@@ -7,7 +7,22 @@ class Gameboard {
     return Array.from({length: 10}, () => Array(10).fill(null));
   }
 
-  placeShip(ship, coordinates, orientation) {}
+  placeShip(ship, coordinates, orientation = "horizontal") {
+    const [x, y] = coordinates;
+    
+    for (let i = 0; i < ship.length; i++) {
+      let placeX = x;
+      let placeY = y;
+
+      if (orientation == "vertical") {
+        placeX += i;
+      } else {
+        placeY += i;
+      }
+
+      this.board[placeX][placeY] = ship;
+    }
+  }
 }
 
 module.exports = Gameboard;
--- Gameboard/gameboard.test.js
+++ Gameboard/gameboard.test.js
@@ -7,9 +7,19 @@ test('Create a 10x10 layout', () => {
   expect(gb.board[0].length).toBe(10);
 });
 
-test('Place ships on board', () => {
+test('Place ships on board horizontally', () => {
   let gb = new Gameboard();
   let ship = new Ship(2);
   gb.placeShip(ship, [0, 1]);
   expect(gb.board[0][1]).toBe(ship);
+  expect(gb.board[0][2]).toBe(ship);
+});
+
+test('Place ships on board vertically', () => {
+  let gb = new Gameboard();
+  let ship = new Ship(3);
+  gb.placeShip(ship, [0, 9], "vertical");
+  expect(gb.board[0][9]).toBe(ship);
+  expect(gb.board[1][9]).toBe(ship);
+  expect(gb.board[2][9]).toBe(ship);
 });
