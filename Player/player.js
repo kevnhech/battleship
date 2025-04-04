4976c361bf8453b6d6fbe6a82eb41b229fa73699
kevin <kevnhech@gmail.com>
2025-04-04 13:23:34 -0700

    feat: create a function to place ships on the gameboard

--- Player/player.js
+++ Player/player.js
@@ -1,10 +1,16 @@
-const Gameboard = require("../Gameboard/gameboard");
+// const Gameboard = require("../Gameboard/gameboard");
+import Gameboard from "../Gameboard/gameboard.js";
 
 class Player {
   constructor(name) {
     this.name = name;
     this.gameboard = new Gameboard;
   }
+
+  placeShip(ship, coordinates, orientation = "horizontal") {
+    this.gameboard.placeShip(ship, coordinates, orientation);
+  }
 }
 
-module.exports = Player;
+// module.exports = Player;
+export default Player;
