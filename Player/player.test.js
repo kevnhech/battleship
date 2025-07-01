954683f9bf2ddbc72613474728ffe27ad030841b
kevin <kevnhech@gmail.com>
2025-07-01 14:29:49 -0700

    feat: deleted old files

--- Player/player.test.js
+++
@@ -1,7 +0,0 @@
-const Player = require("./player");
-const Gameboard = require("../Gameboard/gameboard");
-
-test("Player has a Gameboard", () => {
-  let player = new Player("Kevin");
-  expect(player).toEqual({ name: "Kevin", gameboard: new Gameboard() });
-});
